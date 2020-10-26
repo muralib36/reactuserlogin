c16bb4d4dadcba4f28e12471d9c9e26fa9511b4f
Boddapadu Murali Krishna <bmkrishna@india.avalon.net>
2020-10-26 12:33:23 +0530

    fix

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,38 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Redirect, Route } from "react-router";
 import "./Login.css";
 
 export default function Login() {
-  const [userid, setUserid] = useState("");
-  const [password, setPassword] = useState("");
+  const [accountId, setUserid] = useState("");
+  const [pswd, setPassword] = useState("");
+  const [userstatus, setUserstatus] = useState(false);
+  const [token, setUsertoken] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUserstatus(true);
+    }
+  }, []);
 
   function validateForm() {
-    return userid.length > 0 && password.length > 0;
+    return accountId.length > 0 && pswd.length > 0;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+  // login the user
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = { accountId, pswd };
+    console.log(user);
+    // send the username and password to the server
+    const response = await axios
+      .post("https://apertum-interview.herokuapp.com/api/user/login", user, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setUsertoken(res.data.token);
+        setUserstatus(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setUserstatus(false);
+      });
+    // store the usertoken in localStoragex
+    localStorage.setItem("token", JSON.stringify(token));
+  };
 
   return (
     <div className="Login">
-    <h1 className="title">Login</h1>
+      <h1 className="title">Login</h1>
       <form onSubmit={handleSubmit}>
         <label>UserId:</label>
         <input
           type="text"
-          value={userid}
+          value={accountId}
           onChange={(e) => setUserid(e.target.value)}
         />
         <label>Password:</label>
         <input
           type="password"
-          value={password}
+          value={pswd}
           onChange={(e) => setPassword(e.target.value)}
         />
         {/* <input type="submit" value="Login"/> */}
-        <button type="submit" disabled={!validateForm()}>Login</button>
+        <button type="submit" disabled={!validateForm()}>
+          Login
+        </button>
       </form>
     </div>
   );
